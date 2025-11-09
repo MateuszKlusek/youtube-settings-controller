15b707b90148a7bf49642a4bbc2ee02f78c34ca2
Mateusz Kłusek <mateuszklusek@outlook.com>
2025-11-10 00:57:39 +0100

    chore: fix readme, add readme update to bump-version script

--- scripts/bump-version.ts
+++ scripts/bump-version.ts
@@ -5,27 +5,47 @@ type JSONWithVersion = {
   version: string;
 };
 
-function bumpVersion(version: string, release: string = "patch"): string {
-  const [major, minor, patch] = version.split(".").map(Number);
+const bumpVersion = (newVersion: string, release: string = "patch"): string => {
+  const [major, minor, patch] = newVersion.split(".").map(Number);
   if (release === "major") return `${major + 1}.0.0`;
   if (release === "minor") return `${major}.${minor + 1}.0`;
   return `${major}.${minor}.${patch + 1}`; // default = patch
-}
+};
 
-function updateVersion(file, newVersion) {
+const updateVersion = (file: string, newVersion: string) => {
   const data = JSON.parse(fs.readFileSync(file, "utf8")) as JSONWithVersion;
   data.version = newVersion;
   fs.writeFileSync(file, JSON.stringify(data, null, 2) + "\n");
   console.log(`Updated ${file} → ${newVersion}`);
-}
+};
 
 const pkg = JSON.parse(
   fs.readFileSync("package.json", "utf8")
 ) as JSONWithVersion;
 const newVersion = bumpVersion(pkg.version, process.argv[2] || "patch");
 
+const updateReadmeVersion = (path: string, newVersion: string) => {
+  const content = fs.readFileSync(path, "utf8");
+
+  const lines = content.split("\n");
+  const newBadgeLine = `[![Version](https://img.shields.io/badge/version-${newVersion}-blue.svg)](https://github.com/mateuszklusek/youtube-settings-controller/releases/tag/v${newVersion})`;
+
+  for (let i = lines.length - 1; i >= 0; i--) {
+    if (lines[i].includes("youtube-settings-controller/releases/tag")) {
+      lines[i] = newBadgeLine;
+      break;
+    }
+  }
+  const updatedContent = lines.join("\n");
+  fs.writeFileSync(path, updatedContent);
+  console.log(`Updated ${path} → ${newVersion}`);
+};
+
+// --------------------------------- main ------------------------------------- //
+
 updateVersion("package.json", newVersion);
 updateVersion("manifest.json", newVersion);
+updateReadmeVersion("README.md", newVersion);
 
 console.log("Committing changes...");
 
